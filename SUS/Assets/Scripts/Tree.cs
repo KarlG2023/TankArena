6ca1fb4f4dcb71c38318b96fec439e21cbc495fa
Ormano <justin.cruceru@epitech.eu>
2023-03-24 23:38:32 +0100

    Final Version

---
+++ SUS/Assets/Scripts/Tree.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using Mirror;
+using UnityEngine;
+
+public class Tree : NetworkBehaviour
+{
+    [ClientCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.CompareTag("Player") && !other.CompareTag("Missile"))
+        {
+            return;
+        }
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        Destroy(gameObject, 1f);
+    }
+}
